15ed1ee060325c88b93fcc5832bb14fdda6d0409
Angel Ponce <78607413+Poncius1@users.noreply.github.com>
2021-02-07 12:42:23 -0600

    Primer dia

---
+++ Arithmetic.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Suma (Plus).
+    int x = 4 + 6;
+    cout << x;
+
+
+    return 0;
+    }
+
+
+    int main()
+    {
+        //Resta (Minus)
+      int x = 10 - 5;
+      cout << x;
+
+
+       return 0;
+    }
+
+
+
+
+    int main()
+    {
+        //Division 
+    int x = 10 % 2;
+    cout << x;
+
+
+       return 0;
+    
+    }
+
+
+int main ()
+{
+    //Multiplicacion con las Propiedades asociativas.
+  int x = (4 + 3) * 7
+  cout << x;
+
+
+
+    return 0;
+}
+
+
+
---
+++ If.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+//El "If" Esu una forma en la que podemos hacer que nuestro programa haga decisiones basicas.
+
+
+int main()
+{
+    //Si la operacion es verdadera se ejecutara el codigo 
+    //Si usamos >= quiere decir: "mayor o igual que"
+if(5>3){
+    cout<< "Poncius is AWESOME!!!";
+
+
+}
+
+
+    return 0;
+}
+
+//Ejemplo 2
+
+int main()
+{
+if(5>=3){
+    cout<< "Poncius is AWESOME!!!";
+  
+}
+
+    return 0;
+}
+
+
+int main()
+{
+    //Si el numero es mayor o igual que 1 el mensaje se mostrara.
+
+if(1<=3){
+    cout<< "Poncius is AWESOME!!!";
+  
+
+}
+
+
+    return 0;
+}
+
+
+
+//Ejemplo 3
+
+
+int main()
+{
+    //Para numeros iguales se colocan dos signos Iguales siempre
+    //Solo se usa uno para declarar variables
+
+if(3==3){
+    cout<< "Poncius is AWESOME!!!";
+  
+
+}
+
+
+    return 0;
+}
+
+
+//Ejemplo 4
+
+int main()
+{
+    //Se refiere a (X) es diferemte o igual que (X)
+    //Solo se usa uno para declarar variables
+
+if(8!=3){
+    cout<< "Poncius is AWESOME!!!";
+  
+
+}
+
+
+    return 0;
+}
